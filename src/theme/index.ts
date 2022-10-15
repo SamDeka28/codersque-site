f82ab4d3ec520be44292bd886b31441d9ea3ad07
Kalyanmoy Borah <borahkalyanmoy@gmail.com>
2022-10-15 15:42:10 +0530

    first commit

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3A0CA3",
+    },
+    secondary: {
+      main: "#F72585",
+    },
+  },
+});
+
+export default theme;
